e1a839f0970d7bc371b265c9188f313918740c78
irfanurislam <mdirfanurislam@gmail.com>
2023-08-21 17:37:19 +0600

    add hotels

--- index.js
+++ index.js
@@ -7,28 +7,16 @@ const port = process.env.port || 1000;
 require("dotenv").config();
 
 //router here
-const categoryRoute = require("./routes/category")
-const tourDetails = require("./routes/tourDetails")
-const topDestinations = require("./routes/TopDestination")
-const allHotels = require("./routes/hotels")
-
-
-
-
-
+const categoryRoute = require("./routes/category");
+const tourDetails = require("./routes/tourDetails");
+const topDestinations = require("./routes/TopDestination");
+const allHotels = require("./routes/hotels");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njebycd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,44 +25,47 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    client.connect();
     //  work on
-
+    const categoryCollection = client.db("tripsureDB").collection("hotels");
     //from routes
-    app.use("/category",categoryRoute);
-    app.use("/tourDetails",tourDetails);
-    app.use("/top-destinations",topDestinations)
-    app.use("/all-hotels",topDestinations)
-   
+    app.use("/category", categoryRoute);
+    app.use("/tourDetails", tourDetails);
+    app.use("/top-destinations", topDestinations);
+    app.use("/all-hotels", topDestinations);
 
+    //extternal hotels add
 
-
-
-
-
-
-
-
+    app.get("/dashboard/addhotels", async (req, res) => {
+      const result = await categoryCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/dashboard/addhotels", async (req, res) => {
+      const newhotels = req.body;
+      const result = await categoryCollection.insertOne(newhotels);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
-  finally {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
 app.get("/", (req, res) => {
-    res.send("Data base connected SuccessFully");
-  });
-  app.listen(port, () => {
-    console.log(`tripsure server running on port ${port}`);
-  });
+  res.send("Data base connected SuccessFully");
+});
+app.listen(port, () => {
+  console.log(`tripsure server running on port ${port}`);
+});
