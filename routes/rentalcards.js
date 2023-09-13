73bcb295bd6ab4841165ed613036e53189e6e8ce
Habibullah Raju <raju.ra423@gmail.com>
2023-09-13 11:16:34 +0600

    i added rentarcar

--- routes/rentalcards.js
+++ routes/rentalcards.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njebycd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -15,6 +15,12 @@ const client = new MongoClient(uri, {
 
 const rentalCardsCollection = client.db("tripsureDB").collection("rentalcars");
 
+router.get("/singleCar:/id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id : new ObjectId(id)}
+  const result = await rentalCardsCollection.findOne(query);
+  res.send(result);
+})
 
 router.get('/', async (req, res) => {
     const result = await rentalCardsCollection.find().toArray();
