f85e5ccdf167b2ad68114663aba82aae6ffebc3b
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-09-22 17:19:09 +0600

    sayem1

--- routes/searchResult.js
+++ routes/searchResult.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const morgan = require("morgan");
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njebycd.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -24,6 +24,19 @@ router.get("/", async (req, res) => {
   };
   console.log(query);
   const result = await destinationCollection.find(query).toArray();
+  res.send(result);
+});
+
+
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await destinationCollection.findOne(query);
+  res.send(result);
+});
+router.get("/", async (req, res) => {
+  const result = await destinationCollection.find().toArray();
   res.send(result);
 });
 
